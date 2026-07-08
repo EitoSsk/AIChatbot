76846084c8fe13b679a056875067de7e5ecebbf5
bm <bulletman813@gmail.com>
2026-07-08 17:00:04 +0900

    #9 Crtl＋Cでの終了処理追加

--- llm/model.py
+++ llm/model.py
@@ -26,20 +26,36 @@ class Model:
 
     # チャットループを開始するメソッド
     def start_chat(self):
-        self.chat_loop()
+        self._chat_loop()
 
     # チャットループの実装
     # ユーザーの入力を受け取り、モデルに渡して応答を生成し、出力します。
     # チャットはCUI上で行われ、ユーザーが 'exit' と入力するまで続きます。
+    # Ctrl+Cで終了することも可能です。動作は'exit'と同じです。
     # 空のメッセージを入力した場合は、再度入力を促します。
-    def chat_loop(self):
-        print("チャットを開始します。終了するには 'exit' と入力してください。")
+    def _chat_loop(self):
+        print("チャットを開始します。終了するには 'exit' か 'Ctrl+C' と入力してください。")
         while True:
-            user_input = input(f"{self.config.user_name}: ")
-            if user_input.lower() == "exit":
-                print("チャットを終了します。")
+            user_input = ""
+            can_close = False
+
+            try:
+                user_input = input(f"{self.config.user_name}: ")
+                if user_input.lower() == "exit":
+                    can_close = True
+            except KeyboardInterrupt:
+                can_close = True
+
+            # 終了判定
+            if (can_close):
+                self._close_chat()
                 break
             if not user_input.strip():
                 continue
-            response = self.chat.send_message(user_input)
-            print(f"{self.config.assistant_name}: {response}")
+            else:
+                response = self.chat.send_message(user_input)
+                print(f"{self.config.assistant_name}: {response}")
+
+    # チャットの終了処理
+    def _close_chat(self):
+        print("チャットを終了します。")
